8f83c13254eb115f32bd0e05eecf3a3ce7af73b0
Xu Xian <xumonkey1992@gmail.com>
2024-10-28 20:39:20 -0700

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Header</div>
+      <div>
+        <div>All topics</div>
+        <div>Profile</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
